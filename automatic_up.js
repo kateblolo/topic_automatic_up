22ad8d4bdabeb4e42f23b440beeb6cf758ae7600
Clément V <clem.vercollier@gmail.com>
2021-11-04 23:31:26 +0100

    Update automatic_up.js

--- automatic_up.js
+++ automatic_up.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Topic automatic up
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://www.jeuxvideo.com/forums/42-51-*
-// @require http://code.jquery.com/jquery-3.3.1.min.js
+// @version      0.2
+// @description  Script pour UP les topics automatiquement
+// @author       kateblolo
+// @match        https://www.jeuxvideo.com/forums/*
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        GM.setValue
 // @grant        GM.getValue
 // ==/UserScript==
@@ -15,8 +14,8 @@ var instant = false;
 
 function start(){
     GM.setValue(titre, true);
-    $('#message_topic').val("Up!");
-    var delMsg = $( "p:contains('Up!')" ).parent().parent().parent().find("span.picto-msg-croix");
+    $('#message_topic').val(":up:");
+    var delMsg = $( "p:has(img[title=':up:'])" ).parent().parent().parent().find("span.picto-msg-croix");
     if(delMsg.length == 0) { $('.btn.btn-poster-msg.datalayer-push.js-post-message').click(); }
     else if ( instant) delMsg.click();
     function sleep (time) {
@@ -31,23 +30,19 @@ function start(){
 }
 function stop(){
     GM.setValue(titre, false);
-    $("#demarrerScript").html("Demarrer");
-    $("#demarrerScript").css('color','green');
+    $("#demarrerScript").html("Start UP");
     document.getElementById("demarrerScript").addEventListener ("click", start, false);
     instant = true;
 }
 
 GM.getValue(titre).then(function(value) {
     if(value){
-        $(".titre-bloc.titre-bloc-forum").append("<button id='demarrerScript'>Stop</button>");
-        $("#demarrerScript").css('color','red');
+        $('.bloc-outils-top').find('.group-two').append("<button class='btn btn-actu-new-list-forum' id='demarrerScript'>Stop UP</button>");
         document.getElementById("demarrerScript").addEventListener ("click", stop, false);
-        console.log("start executé ...");
         start();
     }
     else{
-        $(".titre-bloc.titre-bloc-forum").append("<button id='demarrerScript'>Demarrer</button>")
-        $("#demarrerScript").css('color','green');
+        $('.bloc-outils-top').find('.group-two').append("<button class='btn btn-actu-new-list-forum' id='demarrerScript'>Start UP</button>")
         document.getElementById("demarrerScript").addEventListener ("click", start, false);
         instant = true;
     }
